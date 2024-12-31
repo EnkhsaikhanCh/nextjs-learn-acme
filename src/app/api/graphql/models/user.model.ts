052249947e39bdee45425c283f0eb96eab952826
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-01 03:00:19 +0800

    create user model

---
+++ src/app/api/graphql/models/user.model.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+
+export type User = {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+const UserSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      default: () => uuidv4(),
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const UserModel = model<User>("User", UserSchema);
