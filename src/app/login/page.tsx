d2a8b6b4a321355126a827e666f54a1d6b7f5585
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-10 01:50:42 +0800

    Create page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { ActionButton } from "@/components/ActionButton";
+import { BaseInput } from "@/components/BaseInput";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { Globe, Loader } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { toast, Toaster } from "sonner";
+import { useLoginUserMutation } from "@/generated/graphql";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {},
+  );
+  const [loginUser] = useLoginUserMutation();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    // Form validation
+    const newErrors: { email?: string; password?: string } = {};
+    if (!email) {
+      newErrors.email = "Email is required.";
+    }
+    if (!password) {
+      newErrors.password = "Password is required.";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      setIsSubmitting(false);
+      return;
+    }
+
+    try {
+      const { data } = await loginUser({
+        variables: {
+          input: { email, password },
+        },
+      });
+
+      if (data?.loginUser?.token) {
+        localStorage.setItem("token", data.loginUser.token);
+        toast.success("Амжилттай нэвтэрлээ!");
+        router.push("/dashboard");
+        return { success: true };
+      } else {
+        toast.error("Серверээс хүлээгдэж буй хариу ирээгүй.");
+        return { success: false };
+      }
+    } catch (error) {
+      console.error("Login алдаа:", error);
+      toast.error("Алдаа гарлаа. Дахин оролдоно уу.");
+      return { success: false };
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted md:p-10">
+      <Toaster position="top-center" expand={false} richColors />
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Globe className="h-4 w-4" />
+          </div>
+          Nomad Tech Inc.
+        </Link>
+        <div className={cn("flex flex-col gap-6")}>
+          <Card className="rounded-none sm:rounded-lg">
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">Нэвтрэх</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <div className="grid gap-6">
+                  <div className="grid gap-6">
+                    {/* Email Input */}
+                    <BaseInput
+                      id="email"
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      label="Email"
+                      error={errors.email}
+                    />
+
+                    {/* Password Input */}
+                    <BaseInput
+                      id="password"
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      label="Password"
+                      error={errors.password}
+                      labelExtra={
+                        <Link
+                          href="/forgot-password"
+                          className="rounded-sm text-blue-500 hover:underline"
+                        >
+                          Forgot Password?
+                        </Link>
+                      }
+                    />
+
+                    {/* Login button */}
+                    <ActionButton
+                      type="submit"
+                      disabled={isSubmitting}
+                      label={
+                        isSubmitting
+                          ? "Нэвтрэхийг баталгаажуулж байна..."
+                          : "Нэвтрэх"
+                      }
+                      icon={
+                        isSubmitting ? (
+                          <Loader className="animate-spin font-semibold" />
+                        ) : null
+                      }
+                    />
+                  </div>
+
+                  {/* Signup */}
+                  <div className="text-center text-sm">
+                    Шинэ хэрэглэгч үү?{" "}
+                    <Link
+                      href="/signup"
+                      className="underline underline-offset-4"
+                    >
+                      Бүртгүүлэх
+                    </Link>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
