5538aed5aa45276c8087b39884cd10d46b1a3674
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-12 04:13:47 +0800

    Update auth-utils.ts

--- src/app/api/graphql/utils/auth-utils.ts
+++ src/app/api/graphql/utils/auth-utils.ts
@@ -1,34 +1,36 @@
 import { useRefreshToken } from "../hooks/useRefreshToken";
 
-export const fetchWithRefresh = async (
-  url: string,
-  options: RequestInit = {},
-) => {
-  let response = await fetch(url, {
-    ...options,
-    credentials: "include",
-  });
+export const useFetchWithRefresh = () => {
+  const refreshToken = useRefreshToken();
 
-  if (response.status === 401) {
-    console.log("Access Token expired. Attempting to refresh...");
+  const fetchWithRefresh = async (url: string, options: RequestInit = {}) => {
+    let response = await fetch(url, {
+      ...options,
+      credentials: "include",
+    });
 
-    const refreshToken = useRefreshToken();
-    const { newAccessToken } = await refreshToken();
+    if (response.status === 401) {
+      console.log("Access Token expired. Attempting to refresh...");
 
-    if (newAccessToken) {
-      response = await fetch(url, {
-        ...options,
-        headers: {
-          ...options.headers,
-          Authorization: `Bearer ${newAccessToken}`,
-        },
-        credentials: "include",
-      });
-    } else {
-      console.error("Refresh failed. Redirecting to login...");
-      window.location.href = "/login";
+      const { newAccessToken } = await refreshToken();
+
+      if (newAccessToken) {
+        response = await fetch(url, {
+          ...options,
+          headers: {
+            ...options.headers,
+            Authorization: `Bearer ${newAccessToken}`,
+          },
+          credentials: "include",
+        });
+      } else {
+        console.error("Refresh failed. Redirecting to login...");
+        window.location.href = "/login";
+      }
     }
-  }
 
-  return response;
+    return response;
+  };
+
+  return fetchWithRefresh;
 };
