215cb426dc88595fd5ca3e696f0aae300d3f9efc
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-14 19:05:12 +0800

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,17 @@
 // src/app/dashboard/page.tsx
 "use client";
+
+import { useGetUserByIdQuery } from "@/generated/graphql";
 import { useSession } from "next-auth/react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function Page() {
   const { data: session, status } = useSession();
@@ -13,11 +24,43 @@ export default function Page() {
     return <p>You are not logged in</p>; // Session байхгүй үед
   }
 
+  const userId = session.user?.id;
+
+  const { data, loading, error } = useGetUserByIdQuery({
+    variables: { id: userId },
+  });
+
+  if (loading) {
+    return <p>Loading user details...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching user details: {error.message}</p>;
+  }
+
+  const user = data?.getUserById;
+
   return (
     <main>
       <h1>
-        Welcome, <span className="font-bold">{session.user?.email}</span>!
+        Welcome, <span className="font-bold">{session.user?.email}</span>
       </h1>
+      <Table className="mt-4 w-[500px]">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Email</TableHead>
+            <TableHead>Student ID</TableHead>
+            <TableHead>Role</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">{user?.email}</TableCell>
+            <TableCell>{user?.studentId}</TableCell>
+            <TableCell>{user?.role}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </main>
   );
 }
