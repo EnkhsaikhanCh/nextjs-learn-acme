40455215aa02d479c810cf800e1fe9be30294383
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-17 11:04:15 +0800

    enrollment graphql schema

--- src/app/api/graphql/schemas/index.ts
+++ src/app/api/graphql/schemas/index.ts
@@ -3,9 +3,11 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import { typeDefs as TestTypeDefs } from "./test.schema";
 import { typeDefs as UserTypeDefs } from "./user.schema";
 import { typeDefs as CourseTypeDefs } from "./course.schema";
+import { typeDefs as EnrollmentTypeDefs } from "./enrollment.schema";
 
 export const typeDefs = mergeTypeDefs([
   TestTypeDefs,
   UserTypeDefs,
   CourseTypeDefs,
+  EnrollmentTypeDefs,
 ]);
