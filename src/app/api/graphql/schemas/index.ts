93c5decaa08557c3d3d77ce91fcb4ef9b181ea26
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-19 14:59:06 +0800

    Create section graphql type

--- src/app/api/graphql/schemas/index.ts
+++ src/app/api/graphql/schemas/index.ts
@@ -4,10 +4,12 @@ import { typeDefs as TestTypeDefs } from "./test.schema";
 import { typeDefs as UserTypeDefs } from "./user.schema";
 import { typeDefs as CourseTypeDefs } from "./course.schema";
 import { typeDefs as EnrollmentTypeDefs } from "./enrollment.schema";
+import { typeDefs as SectionTypeDefs } from "./section.schema";
 
 export const typeDefs = mergeTypeDefs([
   TestTypeDefs,
   UserTypeDefs,
   CourseTypeDefs,
   EnrollmentTypeDefs,
+  SectionTypeDefs,
 ]);
---
+++ src/app/api/graphql/schemas/section.schema.ts
@@ -0,0 +1,45 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type Section {
+    _id: ID!
+    courseId: Course!
+    title: String!
+    description: String
+    order: Int!
+    # lessons: [Lesson]
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Query {
+    getSections(courseId: ID!): [Section!]!
+    getSectionById(id: ID!): Section!
+  }
+
+  type Mutation {
+    createSection(input: CreateSectionInput!): Section!
+    updateSection(id: ID!, input: UpdateSectionInput!): Section!
+    deleteSection(id: ID!): Boolean!
+  }
+
+  input CreateSectionInput {
+    courseId: ID!
+    title: String!
+    description: String
+    order: Int!
+  }
+
+  input UpdateSectionInput {
+    title: String
+    description: String
+    order: Int
+  }
+`;
+
+export type CreateSectionInput = {
+  courseId: string;
+  title: string;
+  description: string;
+  order: number;
+};
