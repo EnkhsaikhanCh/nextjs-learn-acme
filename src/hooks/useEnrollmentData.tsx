5c784da9930c13807d6664160cfc92b80c3f3d9d
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-29 01:59:13 +0800

    Update useEnrollmentData.tsx

--- src/hooks/useEnrollmentData.tsx
+++ src/hooks/useEnrollmentData.tsx
@@ -7,12 +7,15 @@ import {
   useUndoLessonCompletionMutation,
 } from "@/generated/graphql";
 import { toast } from "sonner";
+import { useState } from "react";
 
 interface EnrollmentHookProps {
   courseId?: string;
 }
 
 export function useEnrollmentData({ courseId }: EnrollmentHookProps) {
+  const [isLessonActionLoading, setLessonActionLoading] =
+    useState<boolean>(false); // Тодорхой нэртэй болгосон
   const { data: session } = useSession();
   const userId = session?.user?.id;
 
@@ -37,6 +40,7 @@ export function useEnrollmentData({ courseId }: EnrollmentHookProps) {
   // Хичээл дуусгасныг тэмдэглэх
   const handleMarkLessonAsCompleted = async (lessonId: string) => {
     if (!enrollmentData?.getEnrollmentByUserAndCourse?._id || !lessonId) return;
+    setLessonActionLoading(true);
     try {
       const enrollmentId = enrollmentData.getEnrollmentByUserAndCourse._id;
       const res = await markLessonAsCompleted({
@@ -52,12 +56,16 @@ export function useEnrollmentData({ courseId }: EnrollmentHookProps) {
       toast.error(
         `Failed to mark lesson as completed: ${(error as Error).message}`,
       );
+    } finally {
+      setLessonActionLoading(false);
     }
   };
 
   // Хичээл дууссаныг буцаах
   const handleUndoLessonCompletion = async (lessonId: string) => {
     if (!enrollmentData?.getEnrollmentByUserAndCourse?._id || !lessonId) return;
+    setLessonActionLoading(true); // Ачаалал эхэлсэн
+
     try {
       const enrollmentId = enrollmentData.getEnrollmentByUserAndCourse._id;
       const res = await undoLessonCompletion({
@@ -71,6 +79,8 @@ export function useEnrollmentData({ courseId }: EnrollmentHookProps) {
       toast.error(
         `Failed to undo lesson completion: ${(error as Error).message}`,
       );
+    } finally {
+      setLessonActionLoading(false); // Ачаалал эхэлсэн
     }
   };
 
@@ -79,6 +89,7 @@ export function useEnrollmentData({ courseId }: EnrollmentHookProps) {
     enrollmentData,
     enrollmentLoading,
     enrollmentError,
+    isLessonActionLoading,
     handleMarkLessonAsCompleted,
     handleUndoLessonCompletion,
   };
