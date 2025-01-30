2776b70514ff4ef2d47d571136229db8ca030b0b
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-01-30 19:19:27 +0800

    fix lint error

--- src/app/api/graphql/models/payment.model.ts
+++ src/app/api/graphql/models/payment.model.ts
@@ -1,4 +1,4 @@
-import { HydratedDocument, model, models, Schema } from "mongoose";
+import { model, models, Schema } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 
 export type Payment = {
