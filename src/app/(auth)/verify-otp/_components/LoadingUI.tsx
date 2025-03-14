c0368377a3ac7d0c00372eb0ad17177935927844
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-14 17:34:58 +0800

    refactor: split main code into features and components

--- src/app/(auth)/verify-otp/_components/LoadingUI.tsx
+++ src/app/(auth)/verify-otp/_components/LoadingUI.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { motion } from "framer-motion";
-import { Globe, LoaderCircle, MailCheck } from "lucide-react";
-import Link from "next/link";
+import { LoaderCircle, MailCheck } from "lucide-react";
 
 export function LoadingUI() {
   return (
@@ -11,15 +10,6 @@ export function LoadingUI() {
       exit={{ opacity: 0, y: 20 }}
       transition={{ duration: 0.3 }}
     >
-      <Link
-        href="/"
-        className="mb-6 flex items-center justify-center gap-2 text-lg font-semibold"
-      >
-        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-          <Globe className="h-4 w-4" />
-        </div>
-        OXON
-      </Link>
       <Card className="min-w-full max-w-md sm:w-[460px]">
         <CardHeader>
           <CardTitle className="flex items-center gap-3 text-2xl font-bold text-foreground/80">
