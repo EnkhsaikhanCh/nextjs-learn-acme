e303cb18b5142b3b91538ef051ba89423d683638
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-16 17:26:22 +0800

    Update redis.ts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,6 +3,21 @@ import Redis from "ioredis";
 import { Redis as UpstashRedis } from "@upstash/redis";
 
 const isDev = process.env.NODE_ENV === "development";
+const isPreview = process.env.VERCEL_ENV === "preview";
+
+if (
+  !isDev &&
+  (!(isPreview
+    ? process.env.UPSTASH_REDIS_REST_URL
+    : process.env.REDIS_PROD_KV_REST_API_URL) ||
+    !(isPreview
+      ? process.env.UPSTASH_REDIS_REST_TOKEN
+      : process.env.REDIS_PROD_KV_REST_API_TOKEN))
+) {
+  throw new Error(
+    "Required Redis environment variables (URL or TOKEN) are missing",
+  );
+}
 
 export const redis = isDev
   ? new Redis(process.env.REDIS_URL || "redis://localhost:6379", {
@@ -10,6 +25,10 @@ export const redis = isDev
       maxRetriesPerRequest: 3,
     })
   : new UpstashRedis({
-      url: process.env.UPSTASH_REDIS_URL,
-      token: process.env.UPSTASH_REDIS_TOKEN,
+      url: isPreview
+        ? process.env.UPSTASH_REDIS_REST_URL
+        : process.env.REDIS_PROD_KV_REST_API_URL,
+      token: isPreview
+        ? process.env.UPSTASH_REDIS_REST_TOKEN
+        : process.env.REDIS_PROD_KV_REST_API_TOKEN,
     });
