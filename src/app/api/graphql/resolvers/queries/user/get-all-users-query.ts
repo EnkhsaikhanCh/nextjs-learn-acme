3081aa192d670a76504112148b539ec0f4ed65df
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-18 11:55:38 +0800

    Update get-all-users-query.ts

--- src/app/api/graphql/resolvers/queries/user/get-all-users-query.ts
+++ src/app/api/graphql/resolvers/queries/user/get-all-users-query.ts
@@ -1,7 +1,29 @@
 // src/app/api/graphql/resolver/queries/user/get-all-users.ts
+import { User } from "@/generated/graphql";
 import { UserModel } from "../../../models";
+import { requireAuthAndRoles } from "../../../route";
+import { GraphQLError } from "graphql";
 
-export const getAllUser = async () => {
-  const users = await UserModel.find();
-  return users;
+export const getAllUser = async (
+  _: unknown,
+  __: unknown,
+  context: {
+    user?: User;
+  },
+) => {
+  const { user } = context;
+
+  await requireAuthAndRoles(user, ["ADMIN"]);
+
+  try {
+    const users = await UserModel.find();
+    return users;
+  } catch {
+    throw new GraphQLError("Failed to fetch users", {
+      extensions: {
+        code: "INTERNAL_SERVER_ERROR",
+        http: { status: 500 },
+      },
+    });
+  }
 };
