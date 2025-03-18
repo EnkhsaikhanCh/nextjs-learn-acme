48b04891bbcfc42d90d420b4a72b50f665684dec
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-18 20:09:07 +0800

    Update get-all-course.ts

--- src/app/api/graphql/resolvers/queries/course/get-all-course.ts
+++ src/app/api/graphql/resolvers/queries/course/get-all-course.ts
@@ -1,21 +1,31 @@
 import { GraphQLError } from "graphql";
 import { CourseModel } from "../../../models";
+import { User } from "@/generated/graphql";
+import { requireAuthAndRoles } from "@/lib/auth-utils";
 
-export const getAllCourse = async () => {
+export const getAllCourse = async (
+  _: unknown,
+  __: unknown,
+  context: { user?: User },
+) => {
   try {
-    const courses = await CourseModel.find().populate({
+    const user = context?.user;
+
+    // Хэрэглэгчийн эрхийг шалгах
+    await requireAuthAndRoles(user, ["STUDENT", "ADMIN"]);
+
+    // STUDENT бол зөвхөн PUBLISHED курсуудыг авах
+    const filter = user?.role === "STUDENT" ? { status: "PUBLISHED" } : {};
+
+    // Курсүүдийг авах
+    const courses = await CourseModel.find(filter).populate({
       path: "sectionId",
       model: "Section",
     });
+
     return courses;
-  } catch (error) {
-    if (error instanceof GraphQLError) {
-      throw error;
-    }
-
-    const message = (error as Error).message;
-
-    throw new GraphQLError(`Internal server error: ${message}`, {
+  } catch {
+    throw new GraphQLError("Internal server error", {
       extensions: { code: "INTERNAL_SERVER_ERROR" },
     });
   }
