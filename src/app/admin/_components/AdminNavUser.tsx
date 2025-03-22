2cfb4ab9f6b71307ec498d61cd95e799df864d11
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-22 17:33:48 +0800

    update - admin sidebar

---
+++ src/app/admin/_components/AdminNavUser.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { ChevronUp, CircleUserRound, LogOut, SquareUser } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+
+export function AdminNavUser() {
+  const { isMobile } = useSidebar();
+  const { data: session } = useSession();
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton>
+              <CircleUserRound />
+              {session?.user.email}
+              <ChevronUp className="ml-auto" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel>
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate text-xs">
+                    {session?.user.email}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuItem>
+              <SquareUser />
+              <span>Account</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={async () => {
+                await signOut();
+              }}
+            >
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
