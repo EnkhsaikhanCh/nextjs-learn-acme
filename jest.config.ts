d2d23668de4d70cdc42ed91d9649ee71a1025cc7
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-22 15:15:19 +0800

    update - get-all-users-query.ts

--- jest.config.ts
+++ jest.config.ts
@@ -105,7 +105,6 @@ const config: Config = {
     "src/app/api/graphql/schemas",
     "src/app/api/graphql/resolvers/mutations/enrollment",
     "src/app/api/graphql/resolvers/mutations/course",
-    "src/app/api/graphql/resolvers/queries/user/get-all-users-query.ts",
     "src/app/api/graphql/resolvers/queries/course",
     "src/app/api/graphql/resolvers/queries/enrollment/",
     "src/app/api/graphql/resolvers/queries/section",
