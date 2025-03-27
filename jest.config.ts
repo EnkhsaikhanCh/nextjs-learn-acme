de82c4723053458cf8735812a53a67a19681115d
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-03-27 15:24:21 +0800

    minor ui improvements

--- jest.config.ts
+++ jest.config.ts
@@ -81,6 +81,7 @@ const config: Config = {
     "src/utils/validateLessonInput.ts",
     "src/utils/sanitize.ts",
     "src/utils/validation.ts",
+    "src/utils/slug-to-label.ts",
 
     // src/app/(auth)
     "src/app/\\(auth\\)/forgot-password",
