eb5631fd1756a761f3533c2af9a98bfa6ffe6b51
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-04-06 17:06:06 +0800

    improve code minimize

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -10,9 +10,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { AlertCircle, ArrowLeft, Globe, KeyRound, Loader } from "lucide-react";
+import { AlertCircle, ArrowLeft, KeyRound, Loader } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { PasswordInput } from "@/components/PasswordInput";
 import { useHandlePasswordReset } from "./features/useHandlePasswordReset";
 import { SuccessMessage } from "@/components/SuccessMessage";
@@ -45,118 +44,99 @@ export default function ResetPassword() {
   } = useHandlePasswordReset();
 
   return (
-    <main className="bg-muted flex min-h-svh flex-col items-center gap-6 px-4 md:p-10">
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 20 }}
-        transition={{ duration: 0.3 }}
-      >
-        <Link
-          href="/"
-          className="mb-6 flex items-center justify-center gap-2 text-lg font-semibold"
-        >
-          <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
-            <Globe className="h-4 w-4" />
+    <Card className="max-w-md min-w-full shadow-none">
+      <CardHeader>
+        <CardTitle className="text-foreground/80 flex items-center gap-3 text-xl font-bold md:text-2xl">
+          <div className="flex h-10 w-10 items-center justify-center rounded-md border-2 border-purple-500 bg-purple-200">
+            <KeyRound className="h-6 w-6 stroke-[2.5] text-purple-600" />
+            <span className="sr-only">reset password</span>
           </div>
-          OXON
-        </Link>
-
-        <Card className="max-w-md min-w-full shadow-none">
-          <CardHeader>
-            <CardTitle className="text-foreground/80 flex items-center gap-3 text-xl font-bold md:text-2xl">
-              <div className="flex h-10 w-10 items-center justify-center rounded-md border-2 border-purple-500 bg-purple-200">
-                <KeyRound className="h-6 w-6 stroke-[2.5] text-purple-600" />
-                <span className="sr-only">reset password</span>
-              </div>
-              <p>Нууц үгээ шинэчлэх</p>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="pb-3">
-            {success ? (
-              <>
-                <SuccessMessage description="Таны нууц үг амжилттай шинэчлэгдлээ. Одоо нэвтрэх боломжтой." />
-              </>
-            ) : (
-              <form onSubmit={handlePasswordReset}>
-                <div className="grid w-full items-center gap-4">
-                  {!isChecking ? (
-                    <>
-                      <p className="text-sm text-gray-500">
-                        Шинэ нууц үгээ оруулна уу. Нууц үг нь хамгийн багадаа 8
-                        тэмдэгттэй байх ёстой.
-                      </p>
-                      <div className="grid w-full items-center gap-4">
-                        <PasswordInput
-                          id="new-password"
-                          value={password}
-                          onChange={(e) => {
-                            setPassword(e.target.value);
-                            setFormError(null);
-                          }}
-                          label="Шинэ нууц үг"
-                          placeholder="••••••••"
-                        />
-                        <PasswordInput
-                          id="confirm-password"
-                          value={confirmPassword}
-                          onChange={(e) => {
-                            setConfirmPassword(e.target.value);
-                            setFormError(null);
-                          }}
-                          label="Шинэ нууц үг давтах"
-                          placeholder="••••••••"
-                        />
-                      </div>
-                      {formError && (
-                        <motion.div
-                          initial={{ opacity: 0, y: -10 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          className="flex items-center gap-2 font-semibold text-red-500"
-                        >
-                          <AlertCircle size={16} />
-                          <span className="text-sm">{formError}</span>
-                        </motion.div>
-                      )}
-
-                      <Button
-                        type="submit"
-                        className="mt-3 w-full"
-                        disabled={isLoading}
-                      >
-                        {isLoading ? (
-                          <>
-                            Нууц үг шинэчилж байна...
-                            <Loader className="mr-2 h-4 w-4 animate-spin" />
-                          </>
-                        ) : (
-                          "Нууц үг шинэчлэх"
-                        )}
-                      </Button>
-                    </>
-                  ) : (
-                    <CardLoader />
+          <p>Нууц үгээ шинэчлэх</p>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="pb-3">
+        {success ? (
+          <>
+            <SuccessMessage description="Таны нууц үг амжилттай шинэчлэгдлээ. Одоо нэвтрэх боломжтой." />
+          </>
+        ) : (
+          <form onSubmit={handlePasswordReset}>
+            <div className="grid w-full items-center gap-4">
+              {!isChecking ? (
+                <>
+                  <p className="text-sm text-gray-500">
+                    Шинэ нууц үгээ оруулна уу. Нууц үг нь хамгийн багадаа 8
+                    тэмдэгттэй байх ёстой.
+                  </p>
+                  <div className="grid w-full items-center gap-4">
+                    <PasswordInput
+                      id="new-password"
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                        setFormError(null);
+                      }}
+                      label="Шинэ нууц үг"
+                      placeholder="••••••••"
+                    />
+                    <PasswordInput
+                      id="confirm-password"
+                      value={confirmPassword}
+                      onChange={(e) => {
+                        setConfirmPassword(e.target.value);
+                        setFormError(null);
+                      }}
+                      label="Шинэ нууц үг давтах"
+                      placeholder="••••••••"
+                    />
+                  </div>
+                  {formError && (
+                    <motion.div
+                      initial={{ opacity: 0, y: -10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      className="flex items-center gap-2 font-semibold text-red-500"
+                    >
+                      <AlertCircle size={16} />
+                      <span className="text-sm">{formError}</span>
+                    </motion.div>
                   )}
-                </div>
-              </form>
-            )}
-          </CardContent>
 
-          {/* Only display the footer (login button) when not checking and form is active */}
-          {!isChecking && !success && (
-            <CardFooter className="flex flex-col gap-3">
-              <Button
-                variant="outline"
-                className="w-full"
-                onClick={() => router.push("/login")}
-              >
-                <ArrowLeft className="mr-2 h-4 w-4" />
-                Нэвтрэх хуудас руу буцах
-              </Button>
-            </CardFooter>
-          )}
-        </Card>
-      </motion.div>
-    </main>
+                  <Button
+                    type="submit"
+                    className="mt-3 w-full"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? (
+                      <>
+                        Нууц үг шинэчилж байна...
+                        <Loader className="mr-2 h-4 w-4 animate-spin" />
+                      </>
+                    ) : (
+                      "Нууц үг шинэчлэх"
+                    )}
+                  </Button>
+                </>
+              ) : (
+                <CardLoader />
+              )}
+            </div>
+          </form>
+        )}
+      </CardContent>
+
+      {/* Only display the footer (login button) when not checking and form is active */}
+      {!isChecking && !success && (
+        <CardFooter className="flex flex-col gap-3">
+          <Button
+            variant="outline"
+            className="w-full"
+            onClick={() => router.push("/login")}
+          >
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            Нэвтрэх хуудас руу буцах
+          </Button>
+        </CardFooter>
+      )}
+    </Card>
   );
 }
--- src/app/(auth)/verify-otp/page.tsx
+++ src/app/(auth)/verify-otp/page.tsx
@@ -1,9 +1,7 @@
 // src/app/(auth)/verify-otp/page.tsx
 "use client";
 
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { Globe, Loader, MailCheck } from "lucide-react";
+import { Loader, MailCheck } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { OTPInput } from "@/components/OTPInput";
 import { useOTPVerification } from "./features/useOTPVerification";
@@ -29,122 +27,98 @@ export default function VerifyOTP() {
   } = useOTPVerification();
 
   if (isLoading) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-2 py-12 sm:px-6 lg:px-8">
-        <LoadingUI />
-      </main>
-    );
+    return <LoadingUI />;
   }
 
   if (!email) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-2 py-12 sm:px-6 lg:px-8">
-        <ErrorUI />
-      </main>
-    );
+    return <ErrorUI />;
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-2 py-12 sm:px-6 lg:px-8">
-      <motion.div
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Link
-          href="/"
-          className="mb-6 flex items-center justify-center gap-2 text-lg font-semibold"
-        >
-          <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
-            <Globe className="h-4 w-4" />
-          </div>
-          OXON
-        </Link>
+    <>
+      <Card className="max-w-md min-w-full shadow-none">
+        <CardHeader>
+          <CardTitle className="text-foreground/80 flex items-center gap-3 text-2xl font-bold">
+            <div className="flex h-10 w-10 items-center justify-center rounded-md border-2 border-teal-500 bg-teal-200">
+              <MailCheck className="h-6 w-6 stroke-[2.5] text-teal-600" />
+              <span className="sr-only">Sign up</span>
+            </div>
+            <p>Имэйл баталгаажуулалт</p>
+          </CardTitle>
+        </CardHeader>
 
-        <Card className="max-w-md min-w-full shadow-none">
-          <CardHeader>
-            <CardTitle className="text-foreground/80 flex items-center gap-3 text-2xl font-bold">
-              <div className="flex h-10 w-10 items-center justify-center rounded-md border-2 border-teal-500 bg-teal-200">
-                <MailCheck className="h-6 w-6 stroke-[2.5] text-teal-600" />
-                <span className="sr-only">Sign up</span>
-              </div>
-              <p>Имэйл баталгаажуулалт</p>
+        <CardContent>
+          {success ? (
+            <SuccessMessage description="Таны баталгаажуулалт амжилттай боллоо. Тун удахгүй таныг удирдлагын самбар руу шилжүүлнэ..." />
+          ) : (
+            <>
+              <form onSubmit={handleSubmit} className="space-y-6">
+                <div className="flex flex-col items-center text-center">
+                  {!isVerifying ? (
+                    <OTPInput
+                      length={6}
+                      onComplete={(value) => {
+                        setOtp(value);
+                        handleSubmit();
+                      }}
+                      disabled={isVerifying}
+                      isError={!!error}
+                      onChange={(value) => {
+                        setOtp(value);
+                        setError("");
+                      }}
+                      value={otp}
+                    />
+                  ) : (
+                    <div className="flex h-14 items-center justify-center">
+                      <Loader className="h-8 w-8 animate-spin text-teal-600" />
+                    </div>
+                  )}
+                </div>
+              </form>
+              <p className="text-foreground/60 mt-4 text-center text-sm">
+                6 оронтой код таны <strong>{email}</strong> хаяг руу илгээгдсэн.
+                Код 5 минутын дотор хүчинтэй.
+              </p>
+            </>
+          )}
+        </CardContent>
+      </Card>
+
+      {!success && (
+        <Card className="mt-4 text-center shadow-none">
+          <CardHeader className="py-4 pt-4 pb-0">
+            <CardTitle className="text-md text-foreground/80 font-semibold">
+              Баталгаажуулах код хүлээн аваагүй байна уу?
             </CardTitle>
           </CardHeader>
-
-          <CardContent>
-            {success ? (
-              <SuccessMessage description="Таны баталгаажуулалт амжилттай боллоо. Тун удахгүй таныг удирдлагын самбар руу шилжүүлнэ..." />
-            ) : (
-              <>
-                <form onSubmit={handleSubmit} className="space-y-6">
-                  <div className="flex flex-col items-center text-center">
-                    {!isVerifying ? (
-                      <OTPInput
-                        length={6}
-                        onComplete={(value) => {
-                          setOtp(value);
-                          handleSubmit();
-                        }}
-                        disabled={isVerifying}
-                        isError={!!error}
-                        onChange={(value) => {
-                          setOtp(value);
-                          setError("");
-                        }}
-                        value={otp}
-                      />
-                    ) : (
-                      <div className="flex h-14 items-center justify-center">
-                        <Loader className="h-8 w-8 animate-spin text-teal-600" />
-                      </div>
-                    )}
-                  </div>
-                </form>
-                <p className="text-foreground/60 mt-4 text-center text-sm">
-                  6 оронтой код таны <strong>{email}</strong> хаяг руу
-                  илгээгдсэн. Код 5 минутын дотор хүчинтэй.
-                </p>
-              </>
-            )}
+          <CardContent className="px-4 pb-4">
+            <Button
+              variant="link"
+              type="button"
+              size={"sm"}
+              className={`text-blue-600 transition-opacity hover:underline ${
+                isResending || resendTimer > 0
+                  ? "cursor-not-allowed opacity-50"
+                  : ""
+              }`}
+              onClick={handleResendOTP}
+              disabled={isResending || resendTimer > 0}
+            >
+              {isResending ? (
+                <>
+                  Код дахин илгээж байна...
+                  <Loader className="h-4 w-4 animate-spin" />
+                </>
+              ) : resendTimer > 0 ? (
+                <>Кодыг дахин илгээх ({resendTimer} сек)</>
+              ) : (
+                <>Кодыг дахин илгээх</>
+              )}
+            </Button>
           </CardContent>
         </Card>
-
-        {!success && (
-          <Card className="mt-4 text-center shadow-none">
-            <CardHeader className="py-4 pt-4 pb-0">
-              <CardTitle className="text-md text-foreground/80 font-semibold">
-                Баталгаажуулах код хүлээн аваагүй байна уу?
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="px-4 pb-4">
-              <Button
-                variant="link"
-                type="button"
-                size={"sm"}
-                className={`text-blue-600 transition-opacity hover:underline ${
-                  isResending || resendTimer > 0
-                    ? "cursor-not-allowed opacity-50"
-                    : ""
-                }`}
-                onClick={handleResendOTP}
-                disabled={isResending || resendTimer > 0}
-              >
-                {isResending ? (
-                  <>
-                    Код дахин илгээж байна...
-                    <Loader className="h-4 w-4 animate-spin" />
-                  </>
-                ) : resendTimer > 0 ? (
-                  <>Кодыг дахин илгээх ({resendTimer} сек)</>
-                ) : (
-                  <>Кодыг дахин илгээх</>
-                )}
-              </Button>
-            </CardContent>
-          </Card>
-        )}
-      </motion.div>
-    </main>
+      )}
+    </>
   );
 }
