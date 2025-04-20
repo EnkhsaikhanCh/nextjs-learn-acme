f958f57d56e466b543b97e47e94f3e17e96d025b
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-04-21 03:49:34 +0800

    Update index.ts

--- src/app/api/graphql/resolvers/mutations/lessonV2/index.ts
+++ src/app/api/graphql/resolvers/mutations/lessonV2/index.ts
@@ -1,2 +1,4 @@
 export * from "./create-lesson-v2-mutation";
 export * from "./delete-lesson-v2-mutation";
+export * from "./create-mux-upload-url-mmutation";
+export * from "./update-lesson-v2-mutation";
