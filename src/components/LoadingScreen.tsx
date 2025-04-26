9921b7e28ce9ee684da7b9e840f06dd39e487737
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-04-26 17:25:33 +0800

    Update LoadingScreen.tsx

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -2,9 +2,9 @@ import { Loader } from "lucide-react";
 
 export const LoadingScreen = ({ label }: { label: string }) => {
   return (
-    <div className="flex h-[calc(100vh-4rem)] items-center justify-center">
-      <p className="mr-3 text-lg">{label}</p>
-      <Loader className="h-8 w-8 animate-spin" />
+    <div className="flex h-[calc(100vh-4rem)] items-center justify-center gap-1.5 text-gray-500">
+      <p className="text-sm">{label}</p>
+      <Loader className="h-4 w-4 animate-spin" />
     </div>
   );
 };
