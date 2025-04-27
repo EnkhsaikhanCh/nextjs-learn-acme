778e6a65266fb565a43b32d60bbd1322a2220ca7
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-04-27 08:44:54 +0800

    Update apollo-client.ts

--- src/app/api/graphql/apollo-client.ts
+++ src/app/api/graphql/apollo-client.ts
@@ -1,9 +1,50 @@
 // src/app/api/graphql/apollo-client.ts
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, from } from "@apollo/client";
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
+import { onError } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
 
-const client = new ApolloClient({
+// Error handler link
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      );
+    });
+  }
+  if (networkError) {
+    console.error(`[Network error]: ${networkError}`);
+  }
+});
+
+// Retry link
+const retryLink = new RetryLink({
+  attempts: {
+    max: 3, // 3 удаа дахин оролдоно
+    retryIf: (error) => !!error, // Зөвхөн алдаатай үед retry хийнэ
+  },
+  delay: {
+    initial: 300, // Эхний retry 300ms хүлээнэ
+    max: 1000, // Хамгийн ихдээ 1 секунд хүлээн retry хийнэ
+    jitter: true, // Randomize timing бага зэрэг
+  },
+});
+
+// Batch link
+const batchLink = new BatchHttpLink({
   uri: "/api/graphql",
   credentials: "include",
+  batchMax: 5,
+  batchInterval: 20,
+});
+
+// Apollo Link Chain
+const link = from([errorLink, retryLink, batchLink]);
+
+// Apollo Client
+const client = new ApolloClient({
+  link,
   cache: new InMemoryCache(),
 });
 
