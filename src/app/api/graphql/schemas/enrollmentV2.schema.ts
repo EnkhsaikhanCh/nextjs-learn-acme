3596fc21820d02fe7862019b26c090864b9d23af
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-05-20 19:27:05 +0800

    Create enrollmentV2 graphql schema

---
+++ src/app/api/graphql/schemas/enrollmentV2.schema.ts
@@ -0,0 +1,59 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  scalar Date
+
+  enum EnrollmentV2Status {
+    ACTIVE
+    COMPLETED
+    CANCELLED
+    PENDING
+    EXPIRED
+  }
+
+  type EnrollmentV2 {
+    _id: ID!
+    userId: ID!
+    courseId: ID!
+    status: EnrollmentV2Status!
+    progress: Float
+    isCompleted: Boolean
+    completedLessons: [ID]
+    lastAccessedAt: Date
+    expiryDate: Date
+    history: [EnrollmentV2History]
+    createdAt: Date
+    updatedAt: Date
+    isDeleted: Boolean
+  }
+
+  type EnrollmentV2History {
+    status: EnrollmentStatus!
+    progress: Float!
+    updatedAt: Date!
+  }
+
+  extend type Query {
+    checkEnrollmentV2(courseId: ID!): checkEnrollmentV2Response
+  }
+
+  type checkEnrollmentV2Response {
+    success: Boolean!
+    message: String
+  }
+
+  input CreateEnrollmentV2Input {
+    courseId: ID!
+  }
+
+  extend type Mutation {
+    createEnrollmentV2(
+      input: CreateEnrollmentV2Input!
+    ): EnrollmentV2MutationResponse!
+  }
+
+  type EnrollmentV2MutationResponse {
+    success: Boolean!
+    message: String
+  }
+`;
