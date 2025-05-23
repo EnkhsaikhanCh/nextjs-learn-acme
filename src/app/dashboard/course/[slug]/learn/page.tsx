fd9931a064f53e4293c199076e29d0f6b9e0a343
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2025-05-23 21:49:23 +0800

    Create Enrolled.tsx for learn page

--- src/app/dashboard/course/[slug]/learn/page.tsx
+++ src/app/dashboard/course/[slug]/learn/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { Enrolled } from "@/app/dashboard/courses/[slug]/_components/ENROLLED/Enrolled";
 import { Button } from "@/components/ui/button";
 import { Course, useGetCourseForEnrollmentQuery } from "@/generated/graphql";
 import { ArrowLeft, Loader, TriangleAlert } from "lucide-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import React from "react";
+import { Enrolled } from "../components/Enrolled";
 
 export default function page() {
   const { slug } = useParams();
